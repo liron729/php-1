b8b60c34f4119ad57d46c494319c2b771818cfd7
liron729 <lironaliu8@gmail.com>
2025-09-23 17:42:24 +0200

    :)

---
+++ lesson12/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>digital school</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    
+</body>
+</html>
